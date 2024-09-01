9e0572aad01230ffe8b2f470f9ae29e58b0a012e
Van Tai <vantai08122003@gmail.com>
2024-09-02 00:58:30 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+import * as signalR from '@microsoft/signalr'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  hub:signalR.HubConnection | undefined
+  ngOnInit(): void {
+    console.log("Hello")
+  }
+  constructor(){
+    //this.Connection()
+  }
   title = 'Demo';
+  Connection(){
+    this.hub=new signalR.HubConnectionBuilder().withUrl("https://localhost:7184/chat-hub").build()
+    this.hub?.start().then(()=>{
+      console.log("Connection is started...");
+      this.title="Connection"
+    })
+  }
 }
